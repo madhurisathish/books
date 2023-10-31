d4be7e72f465c5bdb070e05017dc433294dd9fce
madhurisathish <149531019+madhurisathish@users.noreply.github.com>
2023-10-31 12:47:41 -0700

    Update Duplicate_code_example.py

--- Duplicate_code_example.py
+++ Duplicate_code_example.py
@@ -1,20 +1,26 @@
-def calculate_area(length, width):
-    area = length * width
-    return area
-
-def calculate_perimeter(length, width):
-    perimeter = 2 * (length + width)
-    return perimeter
-
-def calculate_volume(length, width, height):
-    volume = length * width * height
-    return volume
-
 def calculate_surface_area(length, width, height):
     return 2 * (length * width + length * height + width * height)
+
+def calculate_area(length, width):
+    return length * width
+
+def calculate_perimeter(length, width):
+    return 2 * (length + width)
 
 def calculate_area_and_perimeter(length, width):
     area = calculate_area(length, width)
     perimeter = calculate_perimeter(length, width)
     return area, perimeter
+
+# Example usage
+length = 5
+width = 3
+height = 2
+
+area_result, perimeter_result = calculate_area_and_perimeter(length, width)
+surface_area_result = calculate_surface_area(length, width, height)
+
+print("Area:", area_result)
+print("Perimeter:", perimeter_result)
+print("Surface Area:", surface_area_result)
 
